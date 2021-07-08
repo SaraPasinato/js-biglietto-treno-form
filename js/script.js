2857c45c5f7ed70af8de4a3b010aaf272b59117c
SaraPasinato <pasinatosara.sp@gmail.com>
2021-07-08 16:51:54 +0200

    fix padding section preview

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@
  * Una casella di testo per il form del passeggero x
  * Una casella numerica per i km                    x
  * Una tendina per selezionare la fascia d'età (Maggiorenne, Minorenne, Over65) x
- *?  NON E' NECESSARIA ALCUNA VALIDAZIONE
- * !Come calcolare  il prezzo:
+ *??  NON E' NECESSARIA ALCUNA VALIDAZIONE ??
+ * !Come calcolare  il prezzo: x
  * il prezzo del biglietto è definito in base ai km (0.21 € al km) 
  * va applicato uno sconto del 20% per i minorenni
  * va applicato uno sconto del 40% per gli over 65.
@@ -17,7 +17,7 @@
  * Tariffa applicata all'utente (Tariffa minori, Tariffa ordinaria, Tariffa Over65)
  * !Per la parte grafica x
  * x  Facciamola solo se tutto il resto della logica è funzionante, vi allego uno screen dell'esercizio fatto in classe senza stile e una da prendere come esempio per la parte visiva. Sentitevi liberi di personalizzare ulteriormente la grafica se vi va.
- * !Bonus
+ * !Bonus X
  * Nascondiamo la sezione del biglietto se non è ancora stato generato il biglietto stesso.
  * Aggiungiamo una funzione che ci permetta di resettare i campi del form ai valori originali.
  */
@@ -73,7 +73,8 @@ btnInsert.addEventListener('click', function () {
 
     price = "€ " + price.toFixed(2);
 
-    // TODO codice treno 
+    // TODO codice treno
+     
 
     //** Stampo gli inner text in html */
     passenger.innerText = valueName;
